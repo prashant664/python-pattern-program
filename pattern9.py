04651d0d84d1fe5e325ffce20f37a796b2709bf2
Prashant <prashantsoni9566@gmail.com>
2021-10-13 05:40:34 -0700

    Initial commit

---
+++ pattern9.py
@@ -0,0 +1,14 @@
+n=int(input("enter the number of rows"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range(n-i):
+        print("@",end=" ")    
+    print()    
+
+for i in range(n):
+    for j in range(n-i):
+         print("@",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()            
